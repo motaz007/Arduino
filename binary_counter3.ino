03b83463bc5dd70a3fd1d5678c0b103c30d55061
motaz007 <motaz.theab@gmail.com>
2017-10-07 17:08:54 +0300

    Add files via upload

---
+++ binary_counter3.ino
@@ -0,0 +1,13 @@
+int count = 0;
+void setup() {
+  // put your setup code here, to run once:
+// using the AVR native language of arduino microcomtroller.
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+PORTD = count;
+count=count +4;
+if (count >124)
+  count=0;
+}
